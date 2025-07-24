2a4ebb4fb79c4db5ffef6dad39e929c2d40397a1
MateusLuciom <luciomateus100@hotmail.com>
2025-07-24 17:32:22 -0300

    Modificação para senha pedida e mensagem de erro com yup

--- backend/middlewares/validate.js
+++ backend/middlewares/validate.js
@@ -6,7 +6,7 @@ function validate(schema) {
       await schema.validate(req.body, { abortEarly: false });
       next();
     } catch (err) {
-      return res.status(400).json({ errors: err.errors });
+      return res.status(400).json({ error: "Erro de validação", details: err.errors });
     }
   };
 }
