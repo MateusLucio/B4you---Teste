e13c8741c89087e577a7b1c3b16ce2f446a695f2
MateusLuciom <luciomateus100@hotmail.com>
2025-07-25 23:55:57 -0300

    Adiciona o docker-compose ao projeto

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
+const db = require("./models/index.js");
 require("dotenv").config();
+
+const connectToDatabase = async () => {
+  db.sequelize
+    .authenticate()
+    .then(() => {
+      console.log("Conexão com o banco de dados estabelecida com sucesso.");
+    })
+    .catch((error) => {
+      setTimeout(() => connectToDatabase(), 5000);
+    });
+};
 
 app.use(express.json());
 
--- backend/models/index.js
+++ backend/models/index.js
@@ -4,10 +4,13 @@ const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const process = require("process");
+const { development } = require("../config/config");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
+const config = require(__dirname + "/../config/config.js")['development'];
 const db = {};
+
+console.log("Config:", config);
 
 let sequelize;
 if (config.use_env_variable) {
